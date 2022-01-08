6dc8b1bbe8484bfb1f00754e46f81357be2cdeb4
HaffiziHasni <86970349+HaffiziHasni@users.noreply.github.com>
2022-01-08 09:09:38 +0800

    Add files via upload

---
+++ App.svelte
@@ -0,0 +1,39 @@
+<style>
+	button{background:#FFC0CB;
+	color:#FFFFFF;
+	left:50%;
+	right:50%;
+	}
+	:global(body) {
+		background-color: #A5E5FA;
+		opacity:0.8;
+	}
+	input{left:50%;
+	right:50%;
+	}
+	
+</style>
+<script>
+	let todos=[];
+	
+	function addToDo(){
+		todos=[...todos,''];
+	}
+	function removeSelf(index){
+		todos=[...todos.slice(0,index),...todos.slice(index+1)];
+	}
+	
+</script>
+<p>
+	Task for the day:
+</p>
+{#each todos as todo, index}
+<input bind:value={todos[index]} placeholder="Enter Task">
+<button on:click={()=>removeSelf(index)}>X
+</button>
+
+
+<br>
+{/each}
+
+<button on:click={addToDo} >Add</button>
